cab6aa81711a4ced30d8211144fde63e04139f1c
TqOliver2 <oliver.gonzalez6@utp.ac.pa>
2025-12-08 18:44:20 -0500

    Primera Version Home solamente

---
+++ Paginas/JS.js
@@ -0,0 +1,23 @@
+// MENU RESPONSIVE
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+// SLIDER AUTOMÁTICO
+const slides = document.querySelectorAll(".carousel-slide");
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach(s => s.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+function autoSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}
+
+setInterval(autoSlide, 3000); // Cada 3s
